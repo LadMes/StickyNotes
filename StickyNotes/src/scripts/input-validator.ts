6ae81b1482dd0246fd4da78d3916d1aa129241d4
LadMes <dartx7x@gmail.com>
2023-11-12 11:42:16 +0400

    Rename property

--- StickyNotes/src/scripts/input-validator.ts
+++ StickyNotes/src/scripts/input-validator.ts
@@ -1,17 +1,17 @@
 ﻿export default class InputValidator {
-  private readonly inputs: Map<HTMLInputElement, ValidationProps>
+  private readonly validations: Map<HTMLInputElement, ValidationProps>
 
   constructor () {
-    this.inputs = new Map<HTMLInputElement, ValidationProps>()
+    this.validations = new Map<HTMLInputElement, ValidationProps>()
     this.handleInputValidation = this.handleInputValidation.bind(this)
   }
 
   validate (): boolean {
-    if (this.inputs.size === 0) {
+    if (this.validations.size === 0) {
       return true
     }
 
-    for (const KVP of this.inputs) {
+    for (const KVP of this.validations) {
       const validationProps = KVP[1]
       if (!validationProps.isValid) {
         return false
@@ -22,22 +22,22 @@
   }
 
   addErrorCondition (input: HTMLInputElement, errorMessage: string, errorCondition: ConditionDelegate): void {
-    this.inputs.set(input, { errorCondition, errorMessage, isValid: false })
+    this.validations.set(input, { errorCondition, errorMessage, isValid: false })
     input.addEventListener('input', this.handleInputValidation)
     input.addEventListener('focusout', this.handleInputValidation)
   }
 
   removeErrorCondition (input: HTMLInputElement): void {
-    if (this.inputs.has(input)) {
+    if (this.validations.has(input)) {
       input.removeEventListener('input', this.handleInputValidation)
       input.removeEventListener('focusout', this.handleInputValidation)
-      this.inputs.delete(input)
+      this.validations.delete(input)
     }
   }
 
   private handleInputValidation (event: Event): void {
     if (event.target instanceof HTMLInputElement) {
-      const validationProps = this.inputs.get(event.target)
+      const validationProps = this.validations.get(event.target)
       if (validationProps !== undefined) {
         if (validationProps.errorCondition()) {
           event.target.classList.add('input-error')
